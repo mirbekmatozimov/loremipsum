5ad828e3a5219c91f04e40c25f42207e586bc521
mirbek <mirbek.25.52@gmail.com>
2021-06-06 17:51:22 +0600

    Updates

---
+++ precache-manifest.3eb0dbf1647766732882fca41aca51d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d3452ad3cfec590085eb6adf09aeb5",
+    "url": "/loremipsum/index.html"
+  },
+  {
+    "revision": "fb08fdc22919d1c8e2ee",
+    "url": "/loremipsum/static/css/main.6a732ffe.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/loremipsum/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loremipsum/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb08fdc22919d1c8e2ee",
+    "url": "/loremipsum/static/js/main.d96778d9.chunk.js"
+  },
+  {
+    "revision": "0867d542d3decfdf195e",
+    "url": "/loremipsum/static/js/runtime-main.bc271619.js"
+  }
+]);
